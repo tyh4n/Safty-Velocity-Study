edcaf8b3d40b2cd7cc06d179dbb3696eafc0ff1a
Han <tianyih4@illinois.edu>
2024-11-12 17:33:34 -0600

    [SW] 11/12 Setting up frame translation and parameters

--- payload_simulation_main.m
+++ payload_simulation_main.m
@@ -20,9 +20,9 @@ d_seat = 0.4;       % Seat depth [m]
 
 % Contact Model
 k_gy = 80000;       % vertical ground interaction stiffness [N/m]
-v_gy_max = 0.03;    % maximum vertical ground relaxation speed [m/s]
+v_gy_max = 0.1;    % maximum vertical ground relaxation speed [m/s]
 k_gx = 8000;        % horizontal ground interaction stiffness [N/m]
-v_gx_max = 0.03;    % maximum horizontal ground relaxation speed [m/s]
+v_gx_max = 0.1;    % maximum horizontal ground relaxation speed [m/s]
 mu_stick = 0.9;     % stiction coefficient
 mu_slide = 0.8;     % sliding coefficient
 vLimit = 0.01;      % slip-stic transition velocity [m/s]
